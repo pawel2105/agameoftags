180137b0d0bfbe60ac4849f91c89da0ba5f00f79
Pawel <brand.magnate@gmail.com>
2016-02-23 18:13:17 +0200

    don't import poor performing images

--- app/models/image_importer.rb
+++ app/models/image_importer.rb
@@ -1,9 +1,15 @@
 class ImageImporter
+  def fetch_data
+    # this is where the API call will be made
+  end
+
   def self.import data_hash
     data = data_hash[:data]
     uid, likes, created_time = data[:id], data[:likes][:count], data[:created_time]
 
-    saved_image = Image.create(ig_media_id: uid, ig_publish_time: created_time, number_of_likes: likes)
-    saved_image.create_hashtags(data[:tags])
+    if likes.to_i > 99
+      saved_image = Image.create(ig_media_id: uid, ig_publish_time: created_time, number_of_likes: likes)
+      saved_image.create_hashtags(data[:tags])
+    end
   end
 end
--- spec/models/image_importer_spec.rb
+++ spec/models/image_importer_spec.rb
@@ -1,25 +1,39 @@
 require 'rails_helper'
 
-def data_hash
+def weak_image_data_hash
   { data: { tags: ['one', 'two'], likes: { count: 11 }, created_time: '1337' } }
+end
+
+def strong_image_data_hash
+  { data: { tags: ['one', 'two'], likes: { count: 110 }, created_time: '1337' } }
 end
 
 describe ImageImporter do
   describe "import" do
-    it "saves the image with the correct data" do
-      ImageImporter.import(data_hash)
+    it "saves the image with the correct data for an image with 100 likes or more" do
+      ImageImporter.import(strong_image_data_hash)
       expect(Image.last.ig_publish_time).to eq '1337'
-      expect(Image.last.number_of_likes).to eq 11
+      expect(Image.last.number_of_likes).to eq 110
       expect(Image.last.hashtags.count).to eq 2
     end
 
-    it "saves the hashtags" do
-      ImageImporter.import(data_hash)
+    it "saves the hashtags for an image with 100 likes or more" do
+      ImageImporter.import(strong_image_data_hash)
       expect(Hashtag.count).to eq 2
       expect(Hashtag.first.label).to eq 'one'
       expect(Hashtag.last.label).to eq 'two'
       expect(Hashtag.first.raw_related_hashtags).to eq ['one', 'two']
       expect(Hashtag.last.raw_related_hashtags).to eq ['one', 'two']
+    end
+
+    it "does not import the image for an image with 99 likes or less" do
+      ImageImporter.import(weak_image_data_hash)
+      expect(Image.count).to eq 0
+    end
+
+    it "does not save any hashtags for an image with 99 likes or less" do
+      ImageImporter.import(weak_image_data_hash)
+      expect(Hashtag.count).to eq 0
     end
   end
 end
