64d5e16d5f87e641bf17a49f4aa7a71adcaefc9b
Pawel <brand.magnate@gmail.com>
2016-05-27 10:53:10 +0200

    move puma config file

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,6 +3,6 @@ workers 2
 
 pidfile "/home/deploy/agameoftags/tmp/puma/pid"
 state_path "/home/deploy/agameoftags/tmp/puma/state"
-bind 'unix:/home/deploy/agameoftags/tmp/sockets/puma.sock'
+bind 'unix:///home/deploy/agameoftags/tmp/sockets/puma.sock'
 environment 'production'
 activate_control_app
