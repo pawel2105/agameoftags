4d9bd34bc0d9f19d4c590cc48f061287ad3d824d
Pawel <brand.magnate@gmail.com>
2016-05-27 10:21:01 +0200

    puma configs

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+threads 1, 6
+workers 2
+
+pidfile "/home/deploy/agameoftags/tmp/puma/pid"
+state_path "/home/deploy/agameoftags/tmp/puma/state"
+environment 'production'
+daemonize true
+bind 'unix:/home/deploy/agameoftags/tmp/sockets/puma.sock'
+environment 'production'
+activate_control_app
